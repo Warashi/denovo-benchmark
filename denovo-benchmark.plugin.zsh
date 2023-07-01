0da7d4fa071f483d9e25d668df087fa90f5b8042
Shinnosuke Sawada <6warashi9@gmail.com>
2023-07-02 07:43:26 +0900

    refactor: rename plugin

--- denovo-benchmark.plugin.zsh
+++ denovo-benchmark.plugin.zsh
@@ -2,5 +2,5 @@ typeset -gaU DENOVO_PATH
 DENOVO_PATH+=("${0:a:h}")
 
 function denovo-benchmark() {
-	denovo-dispatch denovo-benchmark benchmark 64 1000 100 | jq -r .result
+	denovo-dispatch benchmark benchmark 64 1000 100 | jq -r .result
 }
